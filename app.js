0b0456066cda7334af6e44d2fcda70e81b6cd784
Sohaib-GO <s.almajmaie@gmail.com>
2022-08-27 08:26:24 -0600

    new features
    I added the option to delete/edit a post using Mongoos.
    
    I also changed the CSS/Bootstrap a bit !

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const mongoose = require("mongoose");
 const { stubString } = require("lodash");
 
 const homeStartingContent =
-  "rhthgft platea dictumst vestibulum rhoncus est pellentesque platea dictumst vestibulum rhoncus est pellentesque rhoncus est pellentesque. Dictumst vestibulum rhoncus";
+  " Vestibulum vestibulum rhoncus est pellentesque platea dictumst vestibulum rhoncus est pellentesque rhoncus est habitasse platea dictumst vestibulum pellentesque. Dictumst vestibulum rhoncus habitasse platea dictumst vestibulum ";
 const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+  " Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
-  "Scelerisque eleifend donec pretium vulputate rhoncus est pellentesque. Dictumst vestibulum rhoncus  sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+  " pellentesque. Dictumst vestibulum rhoncus. Arcu dui elit  arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const app = express();
 app.set("view engine", "ejs");
 
@@ -48,10 +48,6 @@ app.get("/contact", (req, res) => {
   res.render("contact", { contactInfo: contactContent });
 });
 
-app.get("/compose", (req, res) => {
-  res.render("compose");
-});
-
 // routing
 
 app.get("/posts/:postId", (req, res) => {
@@ -67,9 +63,13 @@ app.get("/posts/:postId", (req, res) => {
   });
 });
 
-
 // New blog route 
-app.post("/compose", (req, res) => {
+
+ app.route("/compose").get((req, res) => {
+  res.render("compose");
+})
+
+.post((req, res) => {
   const post = new Post({
     title: req.body.newPost,
     content: req.body.textArea,
@@ -82,7 +82,11 @@ app.post("/compose", (req, res) => {
   });
 });
 
-// delete route 
+
+
+
+
+// delete post route 
 
 app.post("/delete",  (req, res)=> {
   const deletedPost = req.body.deletedPost;
@@ -92,6 +96,24 @@ app.post("/delete",  (req, res)=> {
     }
   });
 });
+
+// update  title/content route 
+
+app.post("/update",(req, res)=>{
+  const updateId= req.body.updatePost
+  const newTitleName= req.body.newTitle
+  const newContent= req.body.newContent
+
+  Post.updateOne({updateId}, { title: newTitleName,content:newContent },  (err)=> {
+    if (!err) {
+      res.redirect("/posts/"+ updateId);}
+    
+    })
+
+
+})
+
+
 
 
 app.listen(3000,  () =>{
